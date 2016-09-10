437197cc3c7e04c57dfe547cb4311ea7d1439401
Theja <theja2t@gmail.com>
2016-09-11 02:40:45 +0530

    snapshot

--- web/server.py
+++ web/server.py
@@ -1,6 +1,25 @@
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, request, stream_with_context, jsonify
 from camera import VideoCamera
+import numpy
 # from camera import IPCamera
+
+import json
+from functools import wraps
+from flask import redirect, request, current_app
+
+def support_jsonp(f):
+    """Wraps JSONified output for JSONP"""
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        callback = request.args.get('callback', False)
+        if callback:
+            content = str(callback) + '(' + str(f().data) + ')'
+            return current_app.response_class(content, mimetype='application/json')
+        else:
+            return f(*args, **kwargs)
+    return decorated_function
+
+
 
 app = Flask(__name__)
 
@@ -26,11 +45,21 @@ def video_feed():
 
 @app.route('/emotion_value')
 def emotion_value():
-    return webcam_capture.emotion
+    return Response(webcam_capture.emotion)
 
 @app.route('/test')
+@support_jsonp
 def test():
-    return render_template('test.html')
+    f = {'emotion':numpy.random.randint(0,6)}
+    return jsonify(**f)
+
+@app.route('/stream')
+def streamed_response():
+    def generate():
+        yield 'Hello '
+        yield 'request.args'
+        yield '!'
+    return Response(stream_with_context(generate()))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
